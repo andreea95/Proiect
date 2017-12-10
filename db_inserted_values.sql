6f297f4b580294b53d6390719f371cc7a499e979
andreea95 <32669206+andreea95@users.noreply.github.com>
2017-12-10 12:16:23 +0200

    Update db_inserted_values.sql

--- db_inserted_values.sql
+++ db_inserted_values.sql
@@ -1,4 +1,4 @@
-insert into persoana(nume,porecla,domiciliu,data_de_nastere) values("gigel popscu", "gigi","str matasari nr 12D", "1990-12-12");
+insert into persoana(nume,porecla,domiciliu,data_de_nastere) values("gigel popescu", "gigi","str matasari nr 12D", "1990-12-12");
 insert into persoana(nume,porecla,domiciliu,data_de_nastere) values("aniela tamas", "ani","str matasari nr 76A", "1995-11-09");
 
 insert into contact (persoanaId,telefon,telefon_secundar,e_mail) values(1,"0726818839", "0213121253","gigel.poescu@yahoo.com");
